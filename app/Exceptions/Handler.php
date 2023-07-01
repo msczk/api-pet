7b62a23818c5ad8de4bd97cadc48210430f3adbf
msczk <maxime43200@gmail.com>
2023-07-01 17:20:14 +0200

    Réponse 404 custom

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,8 +2,9 @@
 
 namespace App\Exceptions;
 
-use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class Handler extends ExceptionHandler
 {
@@ -21,10 +22,17 @@ class Handler extends ExceptionHandler
     /**
      * Register the exception handling callbacks for the application.
      */
-    public function register(): void
+    public function register()
     {
-        $this->reportable(function (Throwable $e) {
-            //
+        // On modifier le comportement pour l'exception NotFoundHttpException
+        $this->renderable(function (NotFoundHttpException $e, $request) {
+            // Si la requête contient "api/*"
+            if ($request->is("api/*")) {
+                // On retourne une réponse 404 avec un message en JSON
+                return response()->json([
+                    "message" => "Ressource introuvable"
+                ], 404);
+            }
         });
     }
 }
